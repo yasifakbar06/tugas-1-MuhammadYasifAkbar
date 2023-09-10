12426195f021347baa3351dd22924c3e339d2bfe
Yasif <muyayasif@gmail.com>
2023-09-10 20:55:32 +0700

    tugas-1-MuhammadYasifAkbar

--- main.dart
+++ main.dart
@@ -6,10 +6,13 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int usia = DateTime.now().year - tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021011", "Muhammad Yasif Akbar", 2002);
+  mhs.perkenalan();
 }
+
